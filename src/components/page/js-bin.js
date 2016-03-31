f91f6b5125363f25aaabc11117c61e42fb52f83f
duncan60 <tmduncan60@gmail.com>
2016-03-31 21:46:45 +0800

    feat(jsbin): update jsbin

--- src/components/page/js-bin.js
+++ src/components/page/js-bin.js
@@ -1,10 +1,15 @@
 import React, { PropTypes, Component } from 'react';
 
 class JsBin extends Component {
+    iframe() {
+        return {
+            __html: '<iframe src="//jsbin.com/tijavem/2/edit?html,css,output" width="100%" height="450"></iframe>'
+        };
+    }
     render() {
         return (
             <div>
-                <a className="jsbin-embed" href="//jsbin.com/tijavem/embed?html,css,js,output">JS Bin on jsbin.com</a>
+                <div dangerouslySetInnerHTML={ this.iframe() } />
             </div>
         );
     };
